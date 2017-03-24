b2ff71a3604582dce809c29fcced135d0a1b4d3a
Matthias Luebken <matthias.luebken@gmail.com>
2017-03-24 12:13:59 +0100

    rename example

--- e2etests/e2etests.sh
+++ e2etests/e2etests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "Recommend upgrading outdated version"
-curl -s -X POST -d @e2etests/vertx-web-outdated-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
+echo "Example 1: Update version of one outdated depdendecy"
+curl -s -X POST -d @e2etests/example-1-vertx-web-outdated-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
+
