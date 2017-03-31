9be3c1cb39a08492b2de3e8bdaa6b6d4d6b48530
Matthias Luebken <matthias.luebken@gmail.com>
2017-03-31 13:16:56 +0200

    Similarity scoring

--- e2etests/e2etests.sh
+++ e2etests/e2etests.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 echo "Example 1: Update version of one outdated dependency"
+printf "Similarity: "
+curl -s -X GET -d @e2etests/example-1-vertx-web-outdated-rx-java-effective-pom.xml localhost:8080/recommendation | jq .Similarity
 curl -s -X GET -d @e2etests/example-1-vertx-web-outdated-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
 
 echo "Example 2: Add missing dependency"
+printf "Similarity: "
+curl -s -X GET -d @e2etests/example-2-vertx-web-missing-dependency-effective-pom.xml localhost:8080/recommendation | jq .Similarity
 curl -s -X GET -d @e2etests/example-2-vertx-web-missing-dependency-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
 
 echo "Example 3: Downgrade version of one too new dependency"
+printf "Similarity: "
+curl -s -X GET -d @e2etests/example-3-vertx-web-too-new-rx-java-effective-pom.xml localhost:8080/recommendation | jq .Similarity
 curl -s -X GET -d @e2etests/example-3-vertx-web-too-new-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
