7e88045b246065a4503d20aafd0142e2945249cc
Matthias Luebken <matthias.luebken@gmail.com>
2017-03-31 12:43:29 +0200

    New example

--- e2etests/e2etests.sh
+++ e2etests/e2etests.sh
@@ -3,5 +3,8 @@
 echo "Example 1: Update version of one outdated dependency"
 curl -s -X POST -d @e2etests/example-1-vertx-web-outdated-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
 
-echo "Example 2: Downgrade version of one too new dependency"
-curl -s -X POST -d @e2etests/example-2-vertx-web-too-new-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
+echo "Example 2: Add missing dependency"
+curl -s -X POST -d @e2etests/example-2-vertx-web-missing-dependency-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
+
+echo "Example 3: Downgrade version of one too new dependency"
+curl -s -X POST -d @e2etests/example-3-vertx-web-too-new-rx-java-effective-pom.xml localhost:8080/recommendation | jq .RecommendationItems
