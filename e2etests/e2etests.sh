9d331837e6cbcd7b2dccdd17c73e693867e7ed38
Matthias Luebken <matthias.luebken@gmail.com>
2017-04-04 17:46:11 +0200

    Fixed test fixture

--- e2etests/e2etests.sh
+++ e2etests/e2etests.sh
@@ -7,8 +7,8 @@ curl -s -X GET -d @e2etests/example-1-vertx-web-outdated-rx-java-effective-pom.x
 
 echo "Example 2: Add missing dependency"
 printf "Similarity: "
-curl -s -X GET -d @e2etests/example-2-vertx-web-missing-dependency-effective-pom.xml localhost:8088/recommendation | jq .Similarity
-curl -s -X GET -d @e2etests/example-2-vertx-web-missing-dependency-effective-pom.xml localhost:8088/recommendation | jq .RecommendationItems
+curl -s -X GET -d @e2etests/example-2-vertx-openshift-missing-dependency-effective-pom.xml localhost:8088/recommendation | jq .Similarity
+curl -s -X GET -d @e2etests/example-2-vertx-openshift-missing-dependency-effective-pom.xml localhost:8088/recommendation | jq .RecommendationItems
 
 echo "Example 3: Downgrade version of one too new dependency"
 printf "Similarity: "
