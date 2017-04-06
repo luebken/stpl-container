e1201d8bc62b55a95adfa4e5f99d0931f66dff7c
Matthias Luebken <matthias.luebken@gmail.com>
2017-04-06 11:17:18 +0200

    create reference stacks yamls from effective poms

--- cmd/stplcli/main.go
+++ cmd/stplcli/main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
-	"github.com/luebken/stpl/pkg/stpl/liba"
+	"io/ioutil"
+	"os"
+	"strings"
+	"unicode"
+
+	"github.com/luebken/stpl/pkg/stpl/maven"
+
+	"fmt"
+	"log"
 )
 
 func main() {
-	println("hello from stplcli: " + liba.HelloWorld())
+
+	//hackish import reference stacks from effective poms
+
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) != 2 {
+		log.Printf("usage: stplcli pom2referencestack <FILENAME>")
+		os.Exit(1)
+	}
+	filename := argsWithoutProg[1]
+	//	log.Printf("parsing : %v\n", filename)
+
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	projects := maven.Unmarshal(dat)
+
+	fmt.Printf("\nvar %v = `\n", firstToLower(strings.Replace(projects[0].Name, " ", "", -1)))
+	fmt.Printf("name: %v\n", projects[0].Name)
+	descNoTabs := strings.Replace(projects[0].Description, "\t", "", -1)
+	descNoNewLines := strings.Replace(descNoTabs, "\n", " ", -1)
+	fmt.Printf("description: %#v\n", descNoNewLines)
+	fmt.Print("depdendencies: \n")
+
+	for _, dep := range projects[0].Dependencies.Dependencies {
+		if dep.Scope == "compile" {
+			fmt.Printf("- %v:%v:%v\n", dep.GroupID, dep.ArtifactID, dep.VersionString)
+		}
+	}
+	fmt.Printf("`\n")
+}
+
+func firstToLower(s string) string {
+	a := []rune(s)
+	a[0] = unicode.ToLower(a[0])
+	return string(a)
 }
